51c66628e0ed23713569a04c8853231169997925
Evgeniy Savenko <wayrez27@gmail.com>
2023-04-25 16:15:05 +0300

    changes

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,9 @@ const isWasFirstTouch = ref(false)
 function onClickFirstTouch() {
   isWasFirstTouch.value = true
 }
+setTimeout(() => {
+  isWasFirstTouch.value = true
+}, 4000)
 function onClickOpenSite() {
   window.open('https://promo.danissimo-club.ru/')
 }
@@ -39,84 +42,94 @@ function dragMoveListener(event: any) {
 
 <template>
   <transition name="fade">
-    <div
-      style="transition: 1s all; position: absolute; z-index: 100"
+    <img
       v-show="isWasFirstTouch === false"
       @click="onClickFirstTouch()"
-    >
-      <img src="./assets/first.webp" />
-    </div>
+      style="
+        transition: 1s all;
+        position: absolute;
+        z-index: 100;
+        width: 400px;
+        object-fit: contain;
+        top: 250px;
+      "
+      src="./assets/test.png"
+    />
   </transition>
 
-  <img
-    id="drag-1"
-    data-x="30"
-    data-y="-35"
-    class="draggable sticker one"
-    src="./assets/stickers/1.webp"
-    alt="sticker"
-  />
-  <img
-    id="drag-2"
-    data-x="269"
-    data-y="-743"
-    class="draggable sticker two"
-    src="./assets/stickers/2.webp"
-    alt="sticker"
-  />
-  <img
-    id="drag-3"
-    data-x="-131"
-    data-y="-848"
-    class="draggable sticker three"
-    src="./assets/stickers/3.webp"
-    alt="sticker"
-  />
-  <img
-    id="drag-4"
-    data-x="102"
-    data-y="-1244"
-    class="draggable sticker four"
-    src="./assets/stickers/4.webp"
-    alt="sticker"
-  />
-  <img
-    id="drag-5"
-    data-x="291"
-    data-y="-1692"
-    class="draggable sticker five"
-    src="./assets/stickers/5.webp"
-    alt="sticker"
-  />
-  <img
-    id="drag-6"
-    data-x="-160"
-    data-y="-2195"
-    class="draggable sticker six"
-    src="./assets/stickers/6.webp"
-    alt="sticker"
-  />
-  <img
-    id="drag-7"
-    data-x="337"
-    data-y="-3291"
-    class="draggable sticker seven"
-    src="./assets/stickers/7.webp"
-    alt="sticker"
-  />
-  <img
-    id="drag-8"
-    data-x="-128"
-    data-y="-4240"
-    class="draggable sticker eight"
-    src="./assets/stickers/8.webp"
-    alt="sticker"
-  />
-  <div style="position: absolute; top: 0; z-index: 1">
+  <div style="width: 100vh; height: 100vh">
+    <img
+      id="drag-1"
+      data-x="32"
+      data-y="31"
+      class="draggable sticker one"
+      src="./assets/stickers/1.webp"
+      alt="sticker"
+    />
+    <img
+      id="drag-2"
+      data-x="146"
+      data-y="-35"
+      class="draggable sticker two"
+      src="./assets/stickers/2.webp"
+      alt="sticker"
+    />
+    <img
+      id="drag-3"
+      data-x="-98"
+      data-y="205"
+      class="draggable sticker three"
+      src="./assets/stickers/3.webp"
+      alt="sticker"
+    />
+    <img
+      id="drag-4"
+      data-x="54"
+      data-y="315"
+      class="draggable sticker four"
+      src="./assets/stickers/4.webp"
+      alt="sticker"
+    />
+    <img
+      id="drag-5"
+      data-x="152"
+      data-y="533"
+      class="draggable sticker five"
+      src="./assets/stickers/5.webp"
+      alt="sticker"
+    />
+    <img
+      id="drag-6"
+      data-x="-57"
+      data-y="542"
+      class="draggable sticker six"
+      src="./assets/stickers/6.webp"
+      alt="sticker"
+    />
+    <img
+      id="drag-7"
+      data-x="188"
+      data-y="217"
+      class="draggable sticker seven"
+      src="./assets/stickers/7.webp"
+      alt="sticker"
+    />
+    <img
+      id="drag-8"
+      data-x="-64"
+      data-y="-34"
+      class="draggable sticker eight"
+      src="./assets/stickers/8.webp"
+      alt="sticker"
+    />
     <button @click="onClickOpenSite" action="https://promo.danissimo-club.ru/" class="about">
       ПОДРОБНЕЕ ОБ АКЦИИ
     </button>
-    <img src="./assets/bg.webp" />
+    <img style="width: 400px; object-fit: cover" src="./assets/bg.png" />
+    <img
+      style="left: 0px; position: absolute; bottom: 0px; width: 400px; object-fit: cover"
+      src="./assets/acc.png"
+    />
   </div>
 </template>
 
@@ -124,19 +137,21 @@ function dragMoveListener(event: any) {
 .sticker {
   touch-action: none;
   user-select: none;
+  width: 320px;
 }
 
 .about {
   position: absolute;
-  font-size: 30px;
-  border: 5px solid #592718;
-  bottom: 15%;
+  font-size: 25px;
+  border: 3px solid #592718;
+  bottom: 25%;
   font-weight: 700;
-  left: 200px;
+  left: 45px;
   color: #592718;
-  padding: 24px;
+  padding: 9px;
   background: transparent;
   transition: 0.5s;
+  margin-top: 10px;
 }
 .about:hover {
   color: white;
@@ -144,51 +159,51 @@ function dragMoveListener(event: any) {
 }
 .one {
   cursor: none;
-  position: relative;
+  position: absolute;
   z-index: 10;
-  transform: translate(30.3939px, -35.0834px);
+  transform: translate(31.84px, 30.845px);
 }
 .two {
   cursor: none;
-  position: relative;
-  transform: translate(269px, -743px);
+  position: absolute;
   z-index: 4;
+  transform: translate(146.265px, -35.82px);
 }
 .three {
   cursor: none;
-  position: relative;
-  transform: translate(-131.333px, -848.667px);
+  position: absolute;
   z-index: 3;
+  transform: translate(-98.505px, 204.97px);
 }
 .four {
   cursor: none;
-  position: relative;
-  transform: translate(102.75px, -1244px);
+  position: absolute;
   z-index: 10;
+  transform: translate(53.73px, 315.415px);
 }
 .five {
   cursor: none;
-  position: relative;
+  position: absolute;
   z-index: 2;
-  transform: translate(291px, -1692px);
+  transform: translate(152.235px, 533.32px);
 }
 .six {
   cursor: none;
-  position: relative;
-  transform: translate(-160.667px, -2195.33px);
+  position: absolute;
   z-index: 2;
+  transform: translate(-56.715px, 542.275px);
 }
 .seven {
   cursor: none;
-  position: relative;
-  transform: translate(337.333px, -3291.67px);
+  position: absolute;
   z-index: 2;
+  transform: translate(188.055px, 216.91px);
 }
 .eight {
   cursor: none;
-  position: relative;
-  transform: translate(-128px, -4240px);
+  position: absolute;
   z-index: 7;
+  transform: translate(-63.9908px, -33.8566px);
 }
 
 .fade-enter-active,
